6dfb1dfad1e2bf1bfa4b6a7a1b987277f5010a90
Ilango <rajagopalilango@gmail.com>
2023-12-11 21:20:33 +0530

    📦 NEW: Setup architect for deploying to aws

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
 	ignoredRouteFiles: ['**/.*'],
+	postcss: true,
+	publicPath: '/_static/build/',
+	server: 'server.ts',
+	serverBuildPath: 'server/index.mjs',
 	// appDirectory: "app",
 	// assetsBuildDirectory: "public/build",
-	// publicPath: "/build/",
-	// serverBuildPath: "build/index.js",
+	serverModuleFormat: 'esm',
 	tailwind: true,
 }
